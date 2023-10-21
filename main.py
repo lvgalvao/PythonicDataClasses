84655a60ff625f1b416a3e388c369707ce088c92
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-21 07:51:29 -0300

    com dataclass

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
+from dataclasses import dataclass
+
 """
 Esse é o motivo de DataClasses em Python serem incríveis
 """
+
+@dataclass
 class Pessoa:
-    def __init__(self, nome, skill, idade, level):
-        self.nome = nome
-        self.skill = skill
-        self.idade = idade
+    nome: str
+    skill: str
+    idade: int
+    level:int
 
-    def __str__(self) -> str:
-        return f"Essa é minha classe Pessoa: {self.nome},{self.skill},{self.idade}"
-    
-    def __eq__(self, __value: object) -> bool:
-        if isinstance(__value, self.__class__):
-            return (self.nome == __value.nome) and (self.skill == __value.skill) and (self.idade == __value.idade)
-        return False
+p1 = Pessoa(nome="Luciano", skill="Engenheiro de dados", idade=35, level=30)
+p2 = Pessoa(nome="Riviane", skill="Machine Learning", idade=22, level = 40)
+p3 = Pessoa(nome="Teo", skill="Machine Learning", idade=28, level = 99)
+p4 = Pessoa(nome="Teo", skill="Machine Learning", idade=28, level = 99)
 
-p1 = Pessoa(nome="Luciano", skill="Engenharia", idade=35, level=50)
-p2 = Pessoa(nome="Riviane", skill="Machine Learning", idade=22, level=60)
-p3 = Pessoa(nome="Teo", skill="Machine Learning", idade=30, level=99)
-p4 = Pessoa(nome="Teo", skill="Machine Learning", idade=30, level=98)
-
-print(p3)
-print(p4)
-print(p1 == p3) # False
+print(p1) # Pessoa(nome="Luciano", skill="Engenheiro de dados", idade=35)
+print(p2) # Pessoa(nome="Riviane", skill="Machine Learning", idade=22)
+print(p2 == p3) # False
 print(p3 == p4) # True
