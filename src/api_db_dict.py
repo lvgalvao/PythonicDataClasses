3df9a8c8f08c9e795442c7b815f0cff6966fb0b0
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-23 08:04:10 -0300

    update

--- src/api_db_dict.py
+++ src/api_db_dict.py
@@ -1,35 +1,40 @@
 import requests
 import csv
 
+
 def fetch_characters() -> list:
-    url = f'https://rickandmortyapi.com/api/character/?page=1'
+    url = f"https://rickandmortyapi.com/api/character/?page=1"
     response = requests.get(url)
     data = response.json()
     characters = []
 
-    for item in data['results']:
+    for item in data["results"]:
         character = {
-            'id': item['id'],
-            'name': item['name'],
-            'status': item['status'],
-            'species': item['species'],
-            'gender': item['gender'],
-            'url': item['location']['url']
+            "id": item["id"],
+            "name": item["name"],
+            "status": item["status"],
+            "species": item["species"],
+            "gender": item["gender"],
+            "url": item["location"]["url"],
         }
         characters.append(character)
 
     return characters
 
-def save_to_csv(characters, file_name='characters.csv'):
+
+def save_to_csv(characters, file_name="characters.csv"):
     # Especificando os nomes das colunas para o CSV.
-    field_names = ['id', 'name', 'status', 'species', 'gender', 'url']
-    
-    with open(file_name, mode='w', newline='', encoding='utf-8') as file:
+    field_names = ["id", "name", "status", "species", "gender", "url"]
+
+    with open(file_name, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=field_names)
-        
+
         writer.writeheader()
         for character in characters:
-            writer.writerow(character)  # 'character' já é um dicionário, então podemos passá-lo diretamente.
+            writer.writerow(
+                character
+            )  # 'character' já é um dicionário, então podemos passá-lo diretamente.
+
 
 if __name__ == "__main__":
     lista_de_characters = fetch_characters()
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, HttpUrl
 
+
 class Personagem(BaseModel):
     id: int
     name: str
@@ -7,26 +8,3 @@ class Personagem(BaseModel):
     species: str
     gender: str
     url: HttpUrl  # O campo URL deve ser uma string.
-
-# from dataclasses import dataclass
-# import re
-
-# @dataclass
-# class Personagem:
-#     id: int
-#     name: str
-#     status: str
-#     species: str
-#     gender: str
-#     url: str  # O campo URL deve ser uma string.
-
-#     def __post_init__(self):
-#         # Regex simplificada para validar a maioria das URLs
-#         regex = re.compile(
-#             r'^https?://'  # http:// or https://
-#             r'(?:[a-zA-Z0-9.-]|%[0-9a-fA-F]{2})+'  # domain
-#             r'(?:/[a-zA-Z0-9@:%._+~#?&/=]*)?'  # components of the path, query, fragment
-#             r'$', re.IGNORECASE)
-
-#         if not re.match(regex, self.url):
-#             raise ValueError(f"1 validation error for Personagem, input_value='{self.url}'")  # Adicione aspas para identificar espaços em branco ou strings vazias.
